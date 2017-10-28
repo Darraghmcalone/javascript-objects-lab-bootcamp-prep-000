4174309e05987cbd6e911abfa98b7f53a6fde074
darraghmcalone@hotmail.co.uk <darraghmcalone@hotmail.co.uk>
2017-10-28 12:30:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function deleteFromObjectByKey(object, key){
   delete object.key;
   return key;
 }
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return key;
+}
